5e4ced25d99a0acc85b0c4228cc41caaa735aa05
Asen Chekov <asenchekov@Users-MacBook-Pro.local>
2019-12-11 15:21:01 +0000

    feature: about page and styles

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,55 +1,117 @@
 import React from 'react'
 
 
-export default () => {
-  
+export default () => (
+  <div className="container about">
+    <h1>Something about us</h1>
+    <hr/>
 
-  return (
-    <div className="container about">
-      <h1>Something about us</h1>
-      <hr/>
-      <div className="row"
-        style={{'padding': '50px 20px'}}>
-        <div className="col s6 left-side-about">
-          <p>Streatham Kitchen is a popular</p>
-          <p>intimate restaurant with 14 seats.</p>
-          <br/>
-          <p>Our menu indulges the cravings of</p>
-          <p>both meat-eaters and vegetarians</p>
-          <p>plus a wide range of tapas</p>
-          <br/>
-          <p>At Streatham Kitchen we aim to accommodate all kinds</p>
-          <p>of customers, satisfying many special requests.</p>
-          <p>The restaurant is also a perfect venue for family</p>
-          <p>reunions, birthdays and special occasions.</p>
-          <p>We offer a weekly special and are currently BYOB.</p>
-          <br/>
-          <p>20 Mitcham Lane, London, SW166NN</p>
-          <p>streathamkitchen@gmail.com</p>
-          <p>020 3556 7916</p>
-          <br/>
-          <p>Opening hours:</p>
-          <p>Monday closed</p>
-          <p>Tuesday, Wednseday 16:00 - 22:00</p>
-          <p>Thursday 12:30 - 22:00</p>
-          <p>Friday and Saturday 12:00 - 22:30</p>
-          <p>Sunday 12:45 - 20:30</p>
-        </div>
-        <div className="col s6">
-          <div
-            className="row image1"
-          >
-          </div>
-          <div
-            className="row image2"
-          >
-          </div>
-          <div
-            className="row image3"
-          >
-          </div>
+    <div className="card horizontal">
+
+      <div className="card-image">
+        <img
+          className="about-image"
+          src="/image5.jpeg"
+          alt=""
+        />
+        <img
+          className="about-image"
+          src="/image9.jpeg"
+          alt=""
+        />
+        <img
+          className="about-image"
+          src="/image11.jpeg"
+          alt=""
+        />
+      </div>
+
+      <div className="card-stacked">
+        <div className="card-content valign-wrapper">
+          <p className="">
+            Streatham Kitchen is a popular <br/>
+            intimate restaurant with 14 seats. <br/>
+            Our menu indulges the cravings of <br/>
+            both meat-eaters and vegetarians <br/>
+            plus a wide range of tapas <br/>
+            <br/>
+            <br/>
+            At Streatham Kitchen we aim to accommodate <br/>
+            all kinds of customers, <br/>
+            satisfying many special requests. <br/>
+            The restaurant is also a perfect venue for family <br/>
+            reunions, birthdays and special occasions. <br/>
+            We offer a weekly special and are currently BYOB.<br/>
+            <br/>
+            <br/>
+            20 Mitcham Lane, London, <br/>
+            SW166NN <br/>
+            streathamkitchen@gmail.com <br/>
+            020 3556 7916 <br/>
+            <br/>
+            <br/>
+            Opening hours <br/>
+            Monday closed <br/>
+            Tuesday, Wednseday 16:00 - 22:00 <br/>
+            Thursday 12:30 - 22:00 <br/>
+            Friday and Saturday 12:00 - 22:30 <br/>
+            Sunday 12:45 - 20:30 <br/>
+          </p>
         </div>
       </div>
+
     </div>
-  );
-}
+    {/* <div className="card horizontal">
+
+      <div className="card-image">
+        <img
+          className="about-image"
+          src="/image9.jpeg"
+          alt=""
+        />
+      </div>
+
+      <div className="card-stacked">
+        <div className="card-content">
+          <p>
+            At Streatham Kitchen we aim to accommodate all kinds
+            of customers, satisfying many special requests.
+            The restaurant is also a perfect venue for family
+            reunions, birthdays and special occasions.
+            We offer a weekly special and are currently BYOB.<br/>
+            <br/>
+            20 Mitcham Lane, London, SW166NN
+            streathamkitchen@gmail.com
+            020 3556 7916
+          </p>
+        </div>
+      </div>
+
+    </div>
+    <div className="card horizontal">
+
+      <div className="card-image">
+        <img
+          className="about-image"
+          src="/image11.jpeg"
+          alt=""
+        />
+      </div>
+
+      <div className="card-stacked">
+        <div className="card-content">
+          <p>
+            Opening hours <br/>
+            Monday closed <br/>
+            Tuesday, Wednseday 16:00 - 22:00 <br/>
+            Thursday 12:30 - 22:00 <br/>
+            Friday and Saturday 12:00 - 22:30 <br/>
+            Sunday 12:45 - 20:30 <br/>
+          </p>
+        </div>
+      </div>
+
+    </div> */}
+
+  </div>
+);
